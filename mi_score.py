693fdd9f36aeb79e0731acc18341a633289f5459
Minh Nhat Le <nhatle.stu@gmail.com>
2026-09-16 22:10:10 -0600

    Defined mutual information calculation function

---
+++ mi_score.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import mutual_info_classif
